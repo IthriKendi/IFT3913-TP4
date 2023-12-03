1834920c2829b2d2fce5867c8a1b1ac6ee455c4c
Walid Boudehane <walid.boudehane@umontreal.ca>
2023-12-03 02:22:36 -0500

    finalisation du rapport + Boite Blanche tests

--- TP4/currency-converter-in-java-master/src/test/BoiteBlancheTest.java
+++ TP4/currency-converter-in-java-master/src/test/BoiteBlancheTest.java
@@ -1,7 +1,5 @@
 package test;
 
-public package test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -12,7 +10,23 @@ import java.util.ArrayList;
 
 class BoiteBlancheTests {
 
+	
 	ArrayList<Currency> currencies = Currency.init();
+	// pour tester Currency.convert
+	@Test
+	void testBoiteBlanche0() {
+		Double amount = 100.0;
+        Double exchangeValue = 1.5;
+        
+        Double result = Currency.convert(amount, exchangeValue);
+        Double expectedResult = 150.0;
+        
+        assertEquals(expectedResult, result);
+    
+    
+	}
+	//Test de MainWindow.convert
+	//cas 1 de couverture des arcs du graphe de flot de contrôle 
 	@Test
 	void testBoiteBlanche1() {
 		String currency1 = "US Dollar";
@@ -24,6 +38,7 @@ class BoiteBlancheTests {
     
     assertEquals(expectedResult, result);
 	}
+	//cas 2 de couverture des arcs du graphe de flot de contrôle 
 	@Test
 	void testBoiteBlanche2() {
 		String currency1 = "us dollar";
@@ -35,8 +50,8 @@ class BoiteBlancheTests {
         
         assertEquals(expectedResult, result);
 	}
+	//cas 3 de couverture des arcs du graphe de flot de contrôle && couverture d'instructions .
 	@Test
-	
 	void testBoiteBlanche3() {
 		String currency1 = "US Dollar";
 		String currency2 = "Euro";
@@ -47,6 +62,7 @@ class BoiteBlancheTests {
     
 		assertEquals(expectedResult, result);
     }
+	//cas de couverture des chemins indépendants du graphe de flot de contrôle
 	@Test
 	void testBoiteBlanche4() {
 		String currency1 = "US Dollar";
@@ -58,8 +74,19 @@ class BoiteBlancheTests {
     
 		assertEquals(expectedResult, result);
     }
-	
-	
-
+	//test de Couverture des i-chemins 
+	@Test
+	void testBoiteBlanche5() {
+		
+		Double amount = 100.0;
+		for(int i=0;i<currencies.size();i++) {
+		Double result = MainWindow.convert(currencies.get(i).getName(), currencies.get(i).getName(), currencies, amount);
+		Double expectedResult = 100.0;
     
+		assertEquals(expectedResult, result);}
+    }
+	
+	
+		
+
 }
